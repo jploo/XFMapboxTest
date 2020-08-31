f03c7109e142d322d32d2700956b706a81a253d0
Josh Plooster <josh.plooster@polaris.com>
2020-08-31 12:54:45 -0500

    Initial commit.

---
+++ XFMapboxTest.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Com.Mapbox.Mapboxsdk;
+
+namespace XFMapboxTest.Droid
+{
+    [Activity(Label = "XFMapboxTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            Mapbox.GetInstance(ApplicationContext, "API-KEY-HERE");
+
+            LoadApplication(new App());
+        }
+    }
+}
